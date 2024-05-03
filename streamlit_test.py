69ff5e1383fc0f2d20745d136361f77311d4ea53
wavelolz <rayhsu005@gmail.com>
2024-05-03 12:45:47 +0800

    Remove market closed days from plot
    feat: Originally, the plotly would plot date sequencially, meaning that it would plot the days where market does not open as a white space. This commit removes those space

--- streamlit_test.py
+++ streamlit_test.py
@@ -1,7 +1,7 @@
 
 import time
 import streamlit as st
-st.set_page_config(layout="wide")
+# st.set_page_config(layout="wide")
 import numpy as np
 import pandas as pd
 from FinMind.data import DataLoader
@@ -14,6 +14,7 @@ import mplfinance as mpf
 import plotly.graph_objects as go
 import os
 import json
+
 
 def load_config(db_name):
     dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
@@ -69,6 +70,15 @@ def PrepareData(data):
 
     return candle
 
+def ExtractMarketCloseDate(data):
+    date_l = data["date"].to_list()
+    start_date = datetime.strptime(date_l[0], "%Y-%m-%d")
+    end_date = datetime.strptime(date_l[-1], "%Y-%m-%d")
+    diff = (end_date-start_date).days
+    all_days = [str(start_date+timedelta(days=i)).split(" ")[0] for i in range(diff)]
+    close_days = sorted(set(all_days)-set(date_l))
+    return close_days
+
 
 def FilterDate(candle_data, code):
     if code == 0:
@@ -85,8 +95,10 @@ def FilterDate(candle_data, code):
         return candle_data
     return filter_candle_data
 
-
-
+data = FetchData("s0050")
+data = CleanData(data)
+candle_data_all = PrepareData(data)
+close_days = ExtractMarketCloseDate(candle_data_all)
 
 
 tab_graph, tab_dollar_cost_averaging = st.tabs(["個股走勢", "定期定額實驗"])
@@ -99,9 +111,7 @@ with tab_graph:
         stock_id_l
     )
 
-    data = FetchData(option)
-    data = CleanData(data)
-    candle_data_all = PrepareData(data)
+    
     genre_duration = st.radio(
         "請選擇繪圖日期長度",
         ["1月", "3月", "5月", "1年", "5年", "全部時間"],
@@ -131,7 +141,8 @@ with tab_graph:
         )
 
     fig = go.Figure(data=candle)
-    st.plotly_chart(fig)
+    fig.update_xaxes(rangebreaks=[dict(values=close_days)])
+    st.plotly_chart(fig, use_container_width=True)
 
 with tab_dollar_cost_averaging:
     st.header("這裡做定期定額")
